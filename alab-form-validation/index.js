aa56ecbacbd6ced42c364f6563b42058f11b53a6
Brahim Sioini <besioini@gmail.com>
2023-12-14 17:40:59 -0500

    added login validation

--- alab-form-validation/index.js
+++ alab-form-validation/index.js
@@ -1,4 +1,5 @@
 
+//  Register
 const register = document.getElementById('registration');
 const registerName = register.elements['username'];
 const registerEmail = register.elements['email'];
@@ -6,17 +7,28 @@ const registerPassword = register.elements['password'];
 const registerPasswordCheck = register.elements['passwordCheck']
 const registerTerms = register.elements['terms'];
 
-console.log(registerTerms)
-
-// let usersData = JSON.parse(localStorage.getItem('users')) || [];
-// // Update the userAnswers and roundCounter in localStorage
-// localStorage.setItem('userAnswers', JSON.stringify(userAnswers));
-// // Add the user input to the answers array
-// userAnswers.push(userInput);
+// Login
+const login = document.getElementById('login')
+const loginName = login.elements['username'];
+const loginPassword = login.elements['password'];
 
 
-console.log(userAnswers);
 register.addEventListener('submit', validateRegisterForm);
+login.addEventListener('submit', validateLoginForm);
+
+let usersData = JSON.parse(localStorage.getItem('users')) || [];
+
+function validateLoginForm(e){
+    e.preventDefault();
+
+    let validName = validateLoginName();
+    let validPassword = validateLoginPassword();
+
+    if(validName && validPassword){
+        alert('Success Login!');
+    }
+    console.log('Clicked')
+}
 
 function validateRegisterForm(e){
     e.preventDefault();
@@ -27,13 +39,51 @@ function validateRegisterForm(e){
     let validTerms = validateTerms();
 
     if (validName && validEmail && validPassword && validTerms) {
-        // Form is valid, you can proceed to submit the form or further processing
-        console.log("Form Validated");
-        window.confirm('Allow')
-        alert('Submitted')
+        let newUser = {
+            username: registerName.value.toLowerCase(),
+            email: registerEmail.value.toLowerCase(),
+            password: registerPassword.value 
+        };
+        console.log("Storing user:", newUser);
+        usersData.push(newUser);
+        localStorage.setItem('users', JSON.stringify(usersData));
+        console.log("Local Storage Set: ", JSON.parse(localStorage.getItem('users')));
+        alert('Registration successful!');
+        register.reset(); 
     }
+    console.log(usersData)
 }
 
+function validateLoginName(){
+    if(loginName.value === ''){
+        alert('Please enter you name');
+        loginName.focus();
+        return false;
+    }
+    // else if(
+    // usersData.forEach(element => {
+    //     if(element.userName === loginName.value){
+    //         alert('User Found!')
+    //     }
+    //         alert('User Not Found!, plaese register')
+    // }));
+    let foundUser = usersData.find(user => user.username === loginName.value.toLowerCase());
+    if (!foundUser) {
+        alert('User Not Found!, please register');
+        return false;
+    }
+}
+function validateLoginPassword(){
+    if(loginPassword.value === ''){
+        alert('Please enter your password');
+        return false;
+    }
+    let correctPassword = usersData.find(user => user.password.matches(loginPassword.value))
+    if(!correctPassword){
+        alert('User Not Found!, please register');
+        return false;
+    }
+}
 function validateRegisterName(){
     let registerNameVal = registerName.value;
     if(registerNameVal === ''){
@@ -69,20 +119,38 @@ function validateRegisterEmail(){
 }
 
 function validateRegisterPassword(){
-    // Passwords must be at least 12 characters long.
-    // Passwords must have at least one uppercase and one lowercase letter.
-    // Passwords must contain at least one number.
-    // Passwords must contain at least one special character.
-    // Passwords cannot contain the word "password" (uppercase, lowercase, or mixed).
-    // Passwords cannot contain the username.
-    // Both passwords must match.
     let registerPasswordVal = registerPassword.value;
+    let registerPasswordCheckVal = registerPasswordCheck.value;
+    if(registerPasswordVal === ''){
+        alert('Please enter a password!');
+        registerPassword.focus();
+        return false;
+    }
     if(registerPasswordVal.length < 12){
         alert('Password should be more than 12 characters');
         registerPassword.focus();
         return false;
+    }else if(registerPasswordVal.includes(registerName.value)){
+        alert('Password should not contain username or password');
+        registerPassword.focus();
+        return false;             
+    }else if(!/[A-Z]/.test(registerPasswordVal) || !/[a-z]/.test(registerPasswordVal)){
+        alert('Password must have at least one uppercase and one lowercase letter');
+        registerPassword.focus();
+        return false;        
+    }else if (!/\d/.test(registerPasswordVal)) {
+        alert('Password must contain at least one number');
+        registerPassword.focus();
+        return false;
+    }else if (/password/i.test(registerPasswordVal)) {
+        alert('Password cannot contain the word "password"');
+        registerPassword.focus();
+        return false;
+    }else if (registerPasswordVal !== registerPasswordCheckVal) {
+        alert('Passwords do not match');
+        registerPasswordCheck.focus();
+        return false;
     }
-
 }
 
 function validateTerms(){
